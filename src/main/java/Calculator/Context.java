59e136e25c17edb8e944909a1f101733355b673f
Denis Molin <molind@inbox.ru>
2021-03-21 17:41:06 +0700

    first commi

---
+++ src/main/java/Calculator/Context.java
@@ -0,0 +1,18 @@
+package Calculator;
+
+public class Context {
+    int x;
+    int y;
+    char o;
+    State state;
+
+    public void press (char key){
+
+    }
+
+    public int run (String keys) {
+        for (char key : keys.toCharArray())
+            press(key);
+        return x;
+    }
+}
